4122602b8acd99d6b379fe1b3cdda20361a3f6fb
kavinrog <kavinderroghit.k@gmail.com>
2025-04-08 16:12:13 -0700

    Add simple caluclation

--- Maths.cpp
+++ Maths.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
     int a = 9 ;
     double b = 10 ;
@@ -12,4 +14,12 @@ int main(){
     std::cout << d%a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << b++ << std::endl;
+    double x =10; 
+    double y = 5;
+    std::cout << (x+10)/(3*y) <<std::endl;
+    double income = 95000;
+    double stax =  (income * 0.04);
+    double ctax = (income * 0.02);
+    cout << "state is " << stax << " county is " << ctax <<endl;
+    cout << "Total tax is " << stax+ctax;
 }
