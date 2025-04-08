5508c0afb2bf939667c3c76b6a4b7a5c7cdddaf2
kavinrog <kavinderroghit.k@gmail.com>
2025-04-08 15:35:58 -0700

    Add hello World

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl; // << chaining them like this is the best <<
+    int file_size = 1000;
+    std::cout <<file_size;
+    return 0;
+}
