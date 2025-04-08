87476080d50a5bf7175f0945b5d0983dbe1a4361
kavinrog <kavinderroghit.k@gmail.com>
2025-04-08 15:55:57 -0700

    Add math code

---
+++ Maths.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int a = 9 ;
+    double b = 10 ;
+    int d = 18;
+    int c = a+b;
+    std::cout << c << std::endl;
+    std::cout << a-b << std::endl;
+    std::cout << a*b << std::endl;
+    std::cout << b/a << std::endl;
+    std::cout << d%a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << b++ << std::endl;
+}
