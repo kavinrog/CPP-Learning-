efd11404c33765e5157667c593153817d93733ec
kavinrog <kavinderroghit.k@gmail.com>
2025-04-12 22:58:15 -0700

    Update code

--- LeetCode/LinkedList/Merge Two Lists.cpp
+++ LeetCode/LinkedList/Merge Two Lists.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,23 +12,61 @@
  * };
  */
 
- class Solution {
-    public:
-        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-            if(list1 == nullptr){
-                return list2;
-            }
-            if(list2 == nullptr){
-                return list1;
-            }
-            if(list1->val >= list2->val){
-                list2->next = mergeTwoLists(list1, list2->next);
-                return list2;
-            }
-            else{
-                list1->next = mergeTwoLists(list1->next, list2);
-                return list1;
-            }
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) {
+            return list2;
         }
-    };
-    
+        if (list2 == nullptr) {
+            return list1;
+        }
+        if (list1->val >= list2->val) {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        } else {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+    }
+};
+
+// Helper function to create a linked list from a vector
+ListNode* createLinkedList(const vector<int>& values) {
+    if (values.empty()) return nullptr;
+    ListNode* head = new ListNode(values[0]);
+    ListNode* current = head;
+    for (size_t i = 1; i < values.size(); ++i) {
+        current->next = new ListNode(values[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+// Helper function to print a linked list
+void printLinkedList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Solution solution;
+
+    // Create two test linked lists
+    vector<int> list1Values = {1, 2, 4};
+    vector<int> list2Values = {1, 3, 4};
+    ListNode* list1 = createLinkedList(list1Values);
+    ListNode* list2 = createLinkedList(list2Values);
+
+    // Merge the two lists
+    ListNode* mergedList = solution.mergeTwoLists(list1, list2);
+
+    // Print the merged list
+    cout << "Merged List: ";
+    printLinkedList(mergedList);
+
+    return 0;
+}
